a28fc95f9a082750cc68169c0a0dba7be8cc9db2
Your Name <100946724+ryodmm@users.noreply.github.com>
2022-04-13 06:26:18 +0000

    [Add]sample_app

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,9 +4,12 @@ class ListsController < ApplicationController
     end
 
     def create
-      list = List.new(list_params)
-      list.save
-      redirect_to list_path(list.id)  
+      @list = List.new(list_params)
+      if @list.save
+        redirect_to list_path(@list.id)
+      else
+        render :new
+      end
     end
 
     def index
@@ -14,17 +17,23 @@ class ListsController < ApplicationController
     end
 
     def show
-      @list = List.find(params[:id]) 
+      @list = List.find(params[:id])
     end
 
     def edit
        @list = List.find(params[:id])
     end
-    
+
     def update
       list = List.find(params[:id])
       list.update(list_params)
-      redirect_to list_path(list.id)  
+      redirect_to list_path(list.id)
+    end
+
+    def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
     end
 
     private
